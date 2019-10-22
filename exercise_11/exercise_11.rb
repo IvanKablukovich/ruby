7f0dd2ddb7e0d019b88f06869a2c60f6e89a67a4
Ivan <kablukovich_ir_16@mf.grsu.by>
2019-10-22 12:35:46 +0300

    Exercise 11: Asking Questions

---
+++ exercise_11/exercise_11.rb
@@ -0,0 +1,11 @@
+print "How old are you? "
+#gets.chomp is used to take string input from users. Age is string
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+print "What is your name? "
+name = gets.chomp
+
+puts "Hi #{name}. So, you're #{age} old, #{height} tall and #{weight} heavy."
