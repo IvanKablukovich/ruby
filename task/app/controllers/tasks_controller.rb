b9d5199aade9dd10a1449c55711883eaa7c0f46a
Ivan <kablukovich_ir_16@mf.grsu.by>
2019-11-20 14:27:25 +0300

    Add pundit gem and politics, Add status to task

--- task/app/controllers/tasks_controller.rb
+++ task/app/controllers/tasks_controller.rb
@@ -2,19 +2,39 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tasks = Task.all
+    @tasks = policy_scope(Task) # Task.all
   end
 
   def new
-    @task = Task.new
+    if current_user.admin?
+      @task = Task.new
+    else
+      redirect_to home_path
+      flash[:alert] = 'Access Denied!'
+    end
   end
 
   def show
+    @task = policy_scope(Task).find(params[:id])  # Task.find(params[:id])
+  end
+
+  def edit
     @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
   end
 
   def create
     @task = Task.new(task_params)
+    authorize @task
     if @task.valid?
       @task.save
       flash[:success] = 'Saved!'
@@ -28,6 +48,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:subject, :description, :assignee, :user_id)
+    params.require(:task).permit(:subject, :description, :assignee, :user_id, :status)
   end
 end
--- task/app/models/user.rb
+++ task/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :tasks
 
+
   devise :database_authenticatable, :registerable, :validatable
+
+  def admin?
+    flag
+  end
 end
